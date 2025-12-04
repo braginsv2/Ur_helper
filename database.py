f7d0730ace1214cf47077abfa1a298986ddb58db
braginsv2 <serega2012500@yandex.ru>
2025-12-04 07:53:28 +0700

    Update database.py

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ import random
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
+
 class DatabaseManager:
     def __init__(self, connection_params=None):
         """Инициализация базы данных PostgreSQL"""
@@ -2372,6 +2373,7 @@ def get_agent_fio_by_id(agent_id):
             result = cursor.fetchone()
 
             return result[0] if result else "Неизвестный агент"
+
 
 
 
