36e25344c832b42afd4aa5e5caa22479d34ba216
braginsv2 <serega2012500@yandex.ru>
2025-12-15 11:35:18 +0700

    Update client_agent.py

--- client_agent.py
+++ client_agent.py
@@ -21,7 +21,6 @@ active_callbacks = {}
 callback_lock = threading.Lock()
 db = DatabaseManager()
 
-
 insurance_companies = [
     ('АО "Согаз"', "SOGAZ"),
     ('ПАО СК "Росгосстрах"', "Ros"),
@@ -5140,5 +5139,6 @@ def cleanup_messages(bot, chat_id, message_id, count):
         except:
 
             pass
+
 
 
