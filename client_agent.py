307fdb4b4159bf6b3894456618f4a07686d28c04
braginsv2 <serega2012500@yandex.ru>
2025-12-25 08:08:31 +0700

    Update client_agent.py

--- client_agent.py
+++ client_agent.py
@@ -17,6 +17,7 @@ import json
 import time
 from functools import wraps
 
+
 active_callbacks = {}
 callback_lock = threading.Lock()
 db = DatabaseManager()
@@ -6085,4 +6086,5 @@ def cleanup_messages(bot, chat_id, message_id, count):
         except:
 
             pass
+
 
